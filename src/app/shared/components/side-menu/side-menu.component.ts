d9cf7edd2183bb7de8c9a4e5e5a5f5a6e8ba5b58
Asier Zabala <asier.zabala@softtek.com>
2025-03-14 09:13:25 +0100

    reactive forms app

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideMenuComponent {
+
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter(item => item.path !== '**')
+    .map(item => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`
+    }
+    ));
+
+
+  authMenu: MenuItem[] = [{
+    title: 'Registro', route: './auth'
+  }]
+
+  countryMenu: MenuItem[] = [{
+    title: 'Países', route: './country'
+  }]
+}
