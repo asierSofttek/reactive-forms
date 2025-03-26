44758f04e8e5c76348b893af05e6a57cd43aad02
Asier Zabala <asier.zabala@softtek.com>
2025-03-26 16:06:26 +0100

    add and remove form controls

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -19,17 +19,33 @@ export class DynamicPageComponent {
         ['Metal Gear', Validators.required],
         ['Death Strading', Validators.required],
       ],
-      Validators.minLength(3)
+      Validators.minLength(2)
     ),
   });
+
+  // newFavorite = this.fb.control([])
+  newFavorite = new FormControl('', Validators.required);
+
 
   get favoriteGames() {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
-  isValidFieldInArray(formArray: FormArray, index: number) {
-    return (
-      formArray.controls[index].errors && formArray.controls[index].touched
-    )
+  onAddToFavorite() {
+    if (this.newFavorite.invalid) return;
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorite(index: number) {
+    this.favoriteGames.removeAt(index)
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+
+    this.myForm.markAllAsTouched();
   }
 }
