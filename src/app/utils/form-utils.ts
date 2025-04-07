c94116f16c6b6a4796c1e4ab4a8e730e24f9888c
Asier Zabala <asier.zabala@softtek.com>
2025-04-07 16:13:10 +0200

    project finished

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,8 +1,23 @@
-import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+
+
+async function sleep() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, 2500);
+  })
+}
 
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
   static getTextError(errors: ValidationErrors) {
+
+    console.log(errors);
+
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -11,6 +26,19 @@ export class FormUtils {
           return `Mínimo de ${errors['minlength'].requiredLength} caracteres`
         case 'min':
           return `Valor mínimo de ${errors['min'].min}`
+        case 'email':
+          return 'El valor ingresado no es un correo electrónico'
+        case 'emailTaken':
+          return 'El correo electrónica ya se encuentra registrado'
+        case 'notStrider':
+          return 'No se puede usar el nombre de Strider'
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El correo electrónico no es valido'
+          }
+          return 'Error de patrón contra expresion regular'
+        default:
+          return `Error de validación no controlado ${key}`;
       }
     }
     return null
@@ -37,6 +65,36 @@ export class FormUtils {
     const errors = formArray.controls[index].errors ?? {};
     return FormUtils.getTextError(errors);
   };
+
+
+  static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true };
+    }
+  }
+
+  static async checkingServerResponse(control: AbstractControl): Promise<ValidationErrors | null> {
+    console.log('Validando contra servidor');
+
+    await sleep();
+    const formValue = control.value;
+    if (formValue === 'hola@mundo.com') {
+      return {
+        emailTaken: true
+      };
+    }
+    return null
+  }
+
+
+  static notStrider(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    return value === 'strider' ? { noStrider: true } : null
+  }
+
 }
 
 
